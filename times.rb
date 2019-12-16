f174433ecbedf5916e8954064c3585d67a4db5cc
Benito Galvez <mystwalker131@gmail.com>
2019-12-16 04:26:43 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,14 @@
-def using_times
-	#your code here
+def using_times 
+
+#loop times = 
+
+7.times do 
+  puts "Wingardium Leviosa"
+
+
+
+
 end
-
-
-
-
+#using_times 
+#puts'Wingardium Leviosa'
+end
